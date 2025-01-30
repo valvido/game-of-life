e7a34517c26fd813e425d26afbd88b6855039698
ruizpcarlos <carlos.13.rz@gmail.com>
2025-01-30 01:54:17 +0100

    Added functions to generate an initial grid from an RLE file. See examples in /grids dir

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,46 @@
+#![allow(unused_imports)]
 use wasm_game_of_life::{Universe as NaiveUniverse, Cell as NaiveCell};
 use wasm_game_of_life::sparse_matrix::Universe as SparseUniverse;
 
+use std::env;
+
+mod utils;
+use utils::*;
+
+
 fn main() {
-    let width = 10;
-    let height = 10;
+     /* // Get initial grid file from command line
+    let args: Vec<String> = env::args().collect();
+    let file_name = &args[1];
+     */
 
-    let initial_state = vec![
-        vec![0, 1, 0, 0, 0, 0, 1, 0, 1, 0],
-        vec![0, 0, 1, 0, 1, 1, 0, 1, 0, 0],
-        vec![1, 0, 0, 1, 0, 0, 0, 0, 1, 1],
-        vec![0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
-        vec![0, 1, 1, 0, 0, 0, 0, 1, 0, 1],
-        vec![1, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-        vec![0, 1, 0, 0, 0, 0, 1, 1, 1, 0],
-        vec![0, 0, 0, 1, 1, 0, 0, 0, 0, 1],
-        vec![1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        vec![0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
-    ];
+    let file_name = "blom.rle";
 
-    // Flatten the matrix for sparse implementation
-    let flat_matrix: Vec<u8> = initial_state.iter().flatten().cloned().collect();
+    let file_path = format!("./grids/{}", file_name);
+
+    let flat_matrix =  init_from_file(&file_path);
+
+    let grid_size = (flat_matrix.len() as f64).sqrt().floor() as usize;
 
     // Naive implementation
     println!("Naive Game of Life:");
+    let initial_state = vec_to_matrix(&flat_matrix, grid_size);
     let initial_cells: Vec<NaiveCell> = initial_state
         .iter()
         .flatten()
         .map(|&x| if x == 1 { NaiveCell::Alive } else { NaiveCell::Dead })
         .collect();
 
-    let mut naive_universe = NaiveUniverse::new_with_cells(width, height, initial_cells);
+    
+    let grid_w =  grid_size as u32;
+
+    let mut naive_universe = NaiveUniverse::new_with_cells(grid_w, grid_w, initial_cells);
     naive_universe.run_iterations(10);
 
     let naive_cells = naive_universe.get_cells();
-    for row in 0..height {
-        for col in 0..width {
-            let idx = (row * width + col) as usize;
+    for row in 0..grid_size {
+        for col in 0..grid_size {
+            let idx = (row * grid_size + col) as usize;
             print!("{}", naive_cells[idx]);
         }
         println!();
@@ -43,8 +48,7 @@ fn main() {
 
     // Sparse matrix implementation
     println!("\nSparse Matrix Game of Life:");
-    let mut sparse_universe = SparseUniverse::new_with_matrix(width, height, flat_matrix);
-    sparse_universe.run_iterations(10);
+    let mut sparse_universe = SparseUniverse::new_with_matrix(grid_w, grid_w, flat_matrix);
 
-    println!("{}", sparse_universe.render());
+    sparse_universe.run_iterations(10);
 }
